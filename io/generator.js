fec49ec4c2b7dbaf1f6cd83be895bb7a4e73220e
coloz <coloz999@gmail.com>
2025-03-13 18:20:44 +0800

    更新

--- io/generator.js
+++ io/generator.js
@@ -26,18 +26,37 @@ Arduino.forBlock["io_pin_pwm"] = function (block, generator) {
 Arduino.forBlock["io_pinmode"] = function (block, generator) {
   const pin = generator.valueToCode(block, "PIN", Arduino.ORDER_ATOMIC);
   const mode = generator.valueToCode(block, "MODE", Arduino.ORDER_ATOMIC);
-  return `pinMode(${pin}, ${mode});\n`;
+  
+  // 记录该引脚已经被手动设置了模式
+  if (!generator.pinModeSet) {
+    generator.pinModeSet = new Set();
+  }
+  generator.pinModeSet.add(pin);
+  
+  return `pinMode(${pin}, ${mode});`;
 };
 
 Arduino.forBlock["io_digitalread"] = function (block, generator) {
   const pin = generator.valueToCode(block, "PIN", Arduino.ORDER_ATOMIC);
+  
+  // 如果引脚没有被设置过模式，则自动添加pinMode
+  if (!generator.pinModeSet || !generator.pinModeSet.has(pin)) {
+    generator.addSetup(`pinMode_${pin}`, `pinMode(${pin}, INPUT);`);
+  }
+  
   return [`digitalRead(${pin})`, Arduino.ORDER_FUNCTION_CALL];
 };
 
 Arduino.forBlock["io_digitalwrite"] = function (block, generator) {
   const pin = generator.valueToCode(block, "PIN", Arduino.ORDER_ATOMIC);
   const value = generator.valueToCode(block, "STATE", Arduino.ORDER_ATOMIC);
-  return `digitalWrite(${pin}, ${value});\n`;
+  
+  // 如果引脚没有被设置过模式，则自动添加pinMode
+  if (!generator.pinModeSet || !generator.pinModeSet.has(pin)) {
+    generator.addSetup(`pinMode_${pin}`, `pinMode(${pin}, OUTPUT);`);
+  }
+  
+  return `digitalWrite(${pin}, ${value});`;
 };
 
 Arduino.forBlock["io_analogread"] = function (block, generator) {
@@ -48,5 +67,11 @@ Arduino.forBlock["io_analogread"] = function (block, generator) {
 Arduino.forBlock["io_analogwrite"] = function (block, generator) {
   const pin = generator.valueToCode(block, "PIN", Arduino.ORDER_ATOMIC);
   const value = generator.valueToCode(block, "PWM", Arduino.ORDER_ATOMIC);
-  return `analogWrite(${pin}, ${value});\n`;
+  
+  // 如果引脚没有被设置过模式，则自动添加pinMode
+  if (!generator.pinModeSet || !generator.pinModeSet.has(pin)) {
+    generator.addSetup(`pinMode_${pin}`, `pinMode(${pin}, OUTPUT);`);
+  }
+  
+  return `analogWrite(${pin}, ${value});`;
 };
--- serial/generator.js
+++ serial/generator.js
@@ -11,14 +11,14 @@ Arduino.forBlock["serial_print"] = function (block, generator) {
   const content = Arduino.valueToCode(block, "VAR", Arduino.ORDER_ATOMIC);
   // 如果没有初始化过这个串口，自动添加默认初始化
   ensureSerialBegin(obj, generator);
-  return `${obj}.print(${content});\n`;
+  return `${obj}.print(${content});`;
 };
 
 Arduino.forBlock["serial_println"] = function (block, generator) {
   const obj = block.getFieldValue("SERIAL");
   const content = Arduino.valueToCode(block, "VAR", Arduino.ORDER_ATOMIC);
   ensureSerialBegin(obj, generator);
-  return `${obj}.println(${content});\n`;
+  return `${obj}.println(${content});`;
 };
 
 Arduino.forBlock["serial_read"] = function (block, generator) {
@@ -37,7 +37,7 @@ Arduino.forBlock["serial_available"] = function (block, generator) {
 Arduino.forBlock["serial_flush"] = function (block, generator) {
   const obj = block.getFieldValue("SERIAL");
   ensureSerialBegin(obj, generator);
-  return `${obj}.flush();\n`;
+  return `${obj}.flush();`;
 };
 
 Arduino.forBlock["serial_parseint"] = function (block, generator) {
@@ -51,7 +51,7 @@ Arduino.forBlock["serial_write"] = function (block, generator) {
   const data =
     Arduino.valueToCode(block, "DATA", Arduino.ORDER_ATOMIC) || '\"\"';
   ensureSerialBegin(obj, generator);
-  return `${obj}.write(${data});\n`;
+  return `${obj}.write(${data});`;
 };
 
 Arduino.forBlock["serial_read_string"] = function (block, generator) {
