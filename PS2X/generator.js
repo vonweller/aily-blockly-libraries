56b8ba51d09b7922e4dad0592af813cebe059d97
stao <werewolf_st@hotmail.com>
2025-03-24 17:42:46 +0800

    添加PS2X

---
+++ PS2X/generator.js
@@ -0,0 +1,26 @@
+Arduino.forBlock['ps2x_init'] = function(block, generator) {
+  var clk = block.getFieldValue('CLK');
+  var cmd = block.getFieldValue('CMD');
+  var att = block.getFieldValue('ATT');
+  var dat = block.getFieldValue('DAT');
+  
+  generator.addLibrary('PS2X', '#include <PS2X.h>');
+  generator.addVariable('ps2x', 'PS2X ps2x;');
+  generator.addSetup('ps2x_setup', 'ps2x.config_gamepad(' + clk + ', ' + cmd + ', ' + att + ', ' + dat + ');\n  delay(300);');
+  
+  return '';
+};
+
+Arduino.forBlock['ps2x_read'] = function(block, generator) {
+  return 'ps2x.read_gamepad();\n';
+};
+
+Arduino.forBlock['ps2x_button'] = function(block, generator) {
+  var button = block.getFieldValue('BUTTON');
+  return ['ps2x.Button(' + button + ')', Arduino.ORDER_FUNCTION_CALL];
+};
+
+Arduino.forBlock['ps2x_analog'] = function(block, generator) {
+  var analog = block.getFieldValue('ANALOG');
+  return ['ps2x.Analog(' + analog + ')', Arduino.ORDER_FUNCTION_CALL];
+};
