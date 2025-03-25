f7cc17625dc6ae0ea684a144d18ab7ca1e2890e7
coloz <coloz999@gmail.com>
2025-03-25 12:59:41 +0800

    优化

--- core-serial/generator.js
+++ core-serial/generator.js
@@ -1,7 +1,6 @@
 Arduino.forBlock["serial_begin"] = function (block, generator) {
   const obj = block.getFieldValue("SERIAL");
   const speed = block.getFieldValue("SPEED");
-  // 使用tag标记已经初始化了特定串口
   generator.addSetup(`serial_${obj}_begin`, `${obj}.begin(${speed});`);
   return ``;
 };
@@ -11,14 +10,14 @@ Arduino.forBlock["serial_print"] = function (block, generator) {
   const content = Arduino.valueToCode(block, "VAR", Arduino.ORDER_ATOMIC);
   // 如果没有初始化过这个串口，自动添加默认初始化
   ensureSerialBegin(obj, generator);
-  return `${obj}.print(${content});`;
+  return `${obj}.print(${content});\n`;
 };
 
 Arduino.forBlock["serial_println"] = function (block, generator) {
   const obj = block.getFieldValue("SERIAL");
   const content = Arduino.valueToCode(block, "VAR", Arduino.ORDER_ATOMIC);
   ensureSerialBegin(obj, generator);
-  return `${obj}.println(${content});`;
+  return `${obj}.println(${content});\n`;
 };
 
 Arduino.forBlock["serial_read"] = function (block, generator) {
@@ -37,7 +36,7 @@ Arduino.forBlock["serial_available"] = function (block, generator) {
 Arduino.forBlock["serial_flush"] = function (block, generator) {
   const obj = block.getFieldValue("SERIAL");
   ensureSerialBegin(obj, generator);
-  return `${obj}.flush();`;
+  return `${obj}.flush();\n`;
 };
 
 Arduino.forBlock["serial_parseint"] = function (block, generator) {
@@ -51,7 +50,7 @@ Arduino.forBlock["serial_write"] = function (block, generator) {
   const data =
     Arduino.valueToCode(block, "DATA", Arduino.ORDER_ATOMIC) || '\"\"';
   ensureSerialBegin(obj, generator);
-  return `${obj}.write(${data});`;
+  return `${obj}.write(${data});\n`;
 };
 
 Arduino.forBlock["serial_read_string"] = function (block, generator) {
@@ -63,6 +62,6 @@ Arduino.forBlock["serial_read_string"] = function (block, generator) {
 // 辅助函数，确保串口已被初始化
 function ensureSerialBegin(serialPort, generator) {
   // 使用条件方式添加串口初始化代码，如果同名tag的代码已存在则不会重复添加
-  generator.addSetup(`serial_${serialPort}_begin`, `${serialPort}.begin(9600);`, true);
+  generator.addSetup(`serial_${serialPort}_begin`, `${serialPort}.begin(9600);`);
 }
 
