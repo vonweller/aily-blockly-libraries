51f906fc1949348d8d1b7cf10dc05dfd978e95eb
stao <werewolf_st@hotmail.com>
2025-03-28 01:09:46 +0800

    更新

--- SHT/generator.js
+++ SHT/generator.js
@@ -5,9 +5,13 @@ Arduino.forBlock['sht3x_init'] = function (block, generator) {
 };
 
 Arduino.forBlock['sht3x_read_temperature'] = function (block, generator) {
-    return 'float temperature = sensor.getTemperature();\n';
+    generator.addLibrary('#include <SHT3x.h>', '#include <SHT3x.h>');
+    generator.addVariable('SHT3x sensor', 'SHT3x sensor;');
+    return ['sensor.getTemperature()', Arduino.ORDER_FUNCTION_CALL];
 };
 
 Arduino.forBlock['sht3x_read_humidity'] = function (block, generator) {
-    return 'float humidity = sensor.getHumidity();\n';
+    generator.addLibrary('#include <SHT3x.h>', '#include <SHT3x.h>');
+    generator.addVariable('SHT3x sensor', 'SHT3x sensor;');
+    return ['sensor.getHumidity()', Arduino.ORDER_FUNCTION_CALL];
 };
