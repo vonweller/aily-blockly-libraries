2a04422e8d555cca4057438f888412b54fcb9ed8
stao <werewolf_st@hotmail.com>
2025-04-22 13:39:23 +0800

    update

--- .github/actions/geni18n/index.js
+++ .github/actions/geni18n/index.js
@@ -1,7 +1,9 @@
 const core = require('@actions/core');
 const fs = require('fs').promises;
 const path = require('path');
-const { ChatPromptTemplate, ChatOpenAI, StructuredOutputParser } = require('langchain');
+const { ChatPromptTemplate } = require('langchain').prompts || {};
+const { ChatOpenAI } = require('langchain').chat_models?.openai || {};
+const { StructuredOutputParser } = require('langchain').output_parsers || {};
 const { z } = require('zod'); // 使用 zod 替代 Python 中的 Pydantic
 
 
