0050a8f1ff61aa487187538eceaa0cb12eaa4851
my <1556056588@qq.com>
2025-04-30 18:06:39 +0800

    旋转编码器更新

--- encoder/generator.js
+++ encoder/generator.js
@@ -1,31 +1,34 @@
+function getVariableName(block) {
+  const variableField = block.getField('ENCODER');
+  const variableModel = variableField.getVariable();
+  console.log("name: ", variableModel.name);
+  return variableModel.name;
+}
+
 Arduino.forBlock['encoder_init'] = function(block, generator) {
-  var variable_encoder = block.getFieldValue('ENCODER');
-  var dropdown_pin_a = block.getFieldValue('PIN_A');
-  var dropdown_pin_b = block.getFieldValue('PIN_B');
+  const variable_encoder = getVariableName(block);
+  var dropdown_pin_a = block.getFieldValue('PIN_A') || '2';
+  var dropdown_pin_b = block.getFieldValue('PIN_B') || '3';
   
-  // 添加Encoder库
   generator.addLibrary('Encoder', '#include <Encoder.h>');
   
-  // 添加编码器对象的定义
-  generator.addObject(variable_encoder, 'Encoder ' + variable_encoder + '(' + dropdown_pin_a + ', ' + dropdown_pin_b + ')');
+  generator.addObject(`${variable_encoder}`, `Encoder  ${variable_encoder}(` + dropdown_pin_a + ', ' + dropdown_pin_b + ');');
   
   return '';
 };
 
 Arduino.forBlock['encoder_read'] = function(block, generator) {
-  var variable_encoder = block.getFieldValue('ENCODER');
+  const variable_encoder = getVariableName(block);
   
-  // 生成读取编码器值的代码
   var code = variable_encoder + '.read()';
   
   return [code, Arduino.ORDER_FUNCTION_CALL];
 };
 
 Arduino.forBlock['encoder_write'] = function(block, generator) {
-  var variable_encoder = block.getFieldValue('ENCODER');
+  const variable_encoder = getVariableName(block);
   var value_value = generator.valueToCode(block, 'VALUE', Arduino.ORDER_ATOMIC);
   
-  // 生成设置编码器值的代码
   var code = variable_encoder + '.write(' + value_value + ');\n';
   
   return code;
