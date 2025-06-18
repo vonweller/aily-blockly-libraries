757b7bf422c5a2e4ff3e1ec5e6199fbe2331a996
coloz <coloz999@gmail.com>
2025-06-18 17:40:39 +0800

    更新

--- arduino_keyboard/generator.js
+++ arduino_keyboard/generator.js
@@ -1,43 +1,60 @@
+// 键盘库初始化函数
+function initKeyboardLibrary(generator) {
+  // 如果是ESP32，则调用USB库和USBHIDKeyboard库
+  if (window['boardConfig'].core.indexOf('esp32') > -1) {
+    generator.addMacro('check_esp32_usb_mode', `#ifndef ARDUINO_USB_MODE
+#error This ESP32 SoC has no Native USB interface
+#elif ARDUINO_USB_MODE == 1
+#warning This sketch should be used when USB is in OTG mode
+#endif 
+`);
+    generator.addLibrary('include_USB', '#include "USB.h"');
+    generator.addLibrary('include_USBHIDKeyboard', '#include "USBHIDKeyboard.h"');
+    generator.addObject('USBHIDKeyboard_Keyboard;', 'USBHIDKeyboard Keyboard;');
+    generator.addSetupBegin('keyboard_begin', 'Keyboard.begin();');
+    generator.addSetupBegin('usb_begin', 'USB.begin();');
+  } else {
+    generator.addLibrary('#include <Keyboard.h>', '#include <Keyboard.h>');
+    generator.addSetupBegin('keyboard_begin', 'Keyboard.begin();');
+  }
+}
+
 Arduino.forBlock['keyboard_begin'] = function (block, generator) {
-  generator.addLibrary('#include <Keyboard.h>', '#include <Keyboard.h>');
-  return 'Keyboard.begin();\n';
+  initKeyboardLibrary(generator);
+  return '';
 };
 
 Arduino.forBlock['keyboard_end'] = function (block, generator) {
+  initKeyboardLibrary(generator);
   return 'Keyboard.end();\n';
 };
 
 Arduino.forBlock['keyboard_print'] = function (block, generator) {
-  generator.addLibrary('#include <Keyboard.h>', '#include <Keyboard.h>');
-  generator.addSetupBegin('keyboard_begin', 'Keyboard.begin();');
+  initKeyboardLibrary(generator);
   var text = generator.valueToCode(block, 'TEXT', Arduino.ORDER_ATOMIC) || '\'\\0\'';
   return 'Keyboard.print(' + text + ');\n';
 };
 
 Arduino.forBlock['keyboard_press'] = function (block, generator) {
-  generator.addLibrary('#include <Keyboard.h>', '#include <Keyboard.h>');
-  generator.addSetupBegin('keyboard_begin', 'Keyboard.begin();');
+  initKeyboardLibrary(generator);
   var key = generator.valueToCode(block, 'KEY', Arduino.ORDER_ATOMIC) || '\'\\0\'';
   return 'Keyboard.press(' + processKey(key) + ');\n';
 };
 
 Arduino.forBlock['keyboard_special_key'] = function (block, generator) {
-  generator.addLibrary('#include <Keyboard.h>', '#include <Keyboard.h>');
-  generator.addSetupBegin('keyboard_begin', 'Keyboard.begin();');
+  initKeyboardLibrary(generator);
   var key = block.getFieldValue('KEY');
   return [key, Arduino.ORDER_ATOMIC];
 };
 
 Arduino.forBlock['keyboard_release'] = function (block, generator) {
-  generator.addLibrary('#include <Keyboard.h>', '#include <Keyboard.h>');
-  generator.addSetupBegin('keyboard_begin', 'Keyboard.begin();');
+  initKeyboardLibrary(generator);
   var key = generator.valueToCode(block, 'KEY', Arduino.ORDER_ATOMIC) || '\'\\0\'';
   return 'Keyboard.release(' + processKey(key) + ');\n';
 };
 
 Arduino.forBlock['keyboard_release_all'] = function (block, generator) {
-  generator.addLibrary('#include <Keyboard.h>', '#include <Keyboard.h>');
-  generator.addSetupBegin('keyboard_begin', 'Keyboard.begin();');
+  initKeyboardLibrary(generator);
   return 'Keyboard.releaseAll();\n';
 };
 
--- arduino_mouse/generator.js
+++ arduino_mouse/generator.js
@@ -1,19 +1,37 @@
+// 鼠标库初始化函数
+function initMouseLibrary(generator) {
+  // 如果是ESP32，则调用USB库和USBHIDKeyboard库
+  if (window['boardConfig'].core.indexOf('esp32') > -1) {
+    generator.addMacro('check_esp32_usb_mode', `#ifndef ARDUINO_USB_MODE
+#error This ESP32 SoC has no Native USB interface
+#elif ARDUINO_USB_MODE == 1
+#warning This sketch should be used when USB is in OTG mode
+#endif 
+`);
+    generator.addLibrary('include_USB', '#include "USB.h"');
+    generator.addLibrary('include_USBHIDMouse', '#include "USBHIDMouse.h"');
+    generator.addObject('USBHIDMouse_Mouse;', 'USBHIDMouse Mouse;');
+    generator.addSetupBegin('mouse_begin', 'Mouse.begin();');
+    generator.addSetupBegin('usb_begin', 'USB.begin();');
+  } else {
+    generator.addLibrary('#include <Mouse.h>', '#include <Mouse.h>');
+    generator.addSetupBegin('mouse_begin', 'Mouse.begin();');
+  }
+}
+
 Arduino.forBlock['mouse_begin'] = function (block, generator) {
-  generator.addLibrary('#include <Mouse.h>', '#include <Mouse.h>');
-  generator.addSetupBegin('mouse_begin', 'Mouse.begin();');
+  initMouseLibrary(generator);
   return '';
 };
 
 Arduino.forBlock['mouse_click'] = function (block, generator) {
-  generator.addLibrary('#include <Mouse.h>', '#include <Mouse.h>');
-  generator.addSetupBegin('mouse_begin', 'Mouse.begin();');
+  initMouseLibrary(generator);
   var button = block.getFieldValue('BUTTON');
   return 'Mouse.click(' + button + ');\n';
 };
 
 Arduino.forBlock['mouse_move'] = function (block, generator) {
-  generator.addLibrary('#include <Mouse.h>', '#include <Mouse.h>');
-  generator.addSetupBegin('mouse_begin', 'Mouse.begin();');
+  initMouseLibrary(generator);
   var x = generator.valueToCode(block, 'X', generator.ORDER_ATOMIC) || '0';
   var y = generator.valueToCode(block, 'Y', generator.ORDER_ATOMIC) || '0';
   var wheel = generator.valueToCode(block, 'WHEEL', generator.ORDER_ATOMIC) || '0';
@@ -21,23 +39,21 @@ Arduino.forBlock['mouse_move'] = function (block, generator) {
 };
 
 Arduino.forBlock['mouse_press'] = function (block, generator) {
-  generator.addLibrary('#include <Mouse.h>', '#include <Mouse.h>');
-  generator.addSetupBegin('mouse_begin', 'Mouse.begin();');
+  initMouseLibrary(generator);
   var button = block.getFieldValue('BUTTON');
   return 'Mouse.press(' + button + ');\n';
 };
 
 Arduino.forBlock['mouse_release'] = function (block, generator) {
-  generator.addLibrary('#include <Mouse.h>', '#include <Mouse.h>');
-  generator.addSetupBegin('mouse_begin', 'Mouse.begin();');
+  initMouseLibrary(generator);
   var button = block.getFieldValue('BUTTON');
   return 'Mouse.release(' + button + ');\n';
 };
 
 Arduino.forBlock['mouse_is_pressed'] = function (block, generator) {
-  generator.addLibrary('#include <Mouse.h>', '#include <Mouse.h>');
-  generator.addSetupBegin('mouse_begin', 'Mouse.begin();');
+  initMouseLibrary(generator);
   var button = block.getFieldValue('BUTTON');
   var code = 'Mouse.isPressed(' + button + ')';
   return [code, generator.ORDER_FUNCTION_CALL];
 };
+
